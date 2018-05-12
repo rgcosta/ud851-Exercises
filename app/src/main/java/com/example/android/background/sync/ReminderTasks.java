5903fd60d2f76f77b2471c7ea1d67abd5ad8cbf4
Romulo Costa <rgcostbox@gmail.com>
2018-05-12 13:39:22 -0300

    Complete all tasks from the ToDo list

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -1,14 +1,33 @@
 package com.example.android.background.sync;
 
-// TODO (1) Create a class called ReminderTasks
+// DONE (1) Create a class called ReminderTasks
 
-// TODO (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
+// DONE (2) Create a public static constant String called ACTION_INCREMENT_WATER_COUNT
 
-// TODO (6) Create a public static void method called executeTask
-// TODO (7) Add a Context called context and String parameter called action to the parameter list
+// DONE (6) Create a public static void method called executeTask
+// DONE (7) Add a Context called context and String parameter called action to the parameter list
 
-// TODO (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
+// DONE (8) If the action equals ACTION_INCREMENT_WATER_COUNT, call this class's incrementWaterCount
 
-// TODO (3) Create a private static void method called incrementWaterCount
-// TODO (4) Add a Context called context to the argument list
-// TODO (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+// DONE (3) Create a private static void method called incrementWaterCount
+// DONE (4) Add a Context called context to the argument list
+// DONE (5) From incrementWaterCount, call the PreferenceUtility method that will ultimately update the water count
+
+import android.content.Context;
+
+import com.example.android.background.utilities.PreferenceUtilities;
+
+public class ReminderTasks {
+
+    public static final String ACTION_INCREMENT_WATER_COUNT = "action_increment_water_count";
+
+    public static void executeTask(Context context, String action){
+        if (action.equals(ACTION_INCREMENT_WATER_COUNT)){
+            incrementWaterCount(context);
+        }
+    }
+
+    private static void incrementWaterCount(Context context) {
+        PreferenceUtilities.incrementWaterCount(context);
+    }
+}
